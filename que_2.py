557081caa867df7204df375273eb5704cc1dc4ec
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-10 18:34:55 +0530

    Add files via upload

---
+++ que_2.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print("Addition: ",num1 + num2)
+print("Subtraction: ",num1 - num2)
+print("Multiplication: ",num1 * num2)
+print("Division: ",num1 / num2)
