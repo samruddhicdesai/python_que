4eb7930c3d356ad8a0cdc8b189cbb1181a33645a
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-10 18:34:30 +0530

    Add files via upload

---
+++ que_1.py
@@ -0,0 +1,4 @@
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+college_name = input("Enter College Name: ")
+print(f"My name is {name} , I am {age} years old and My college name is {college_name}.")
