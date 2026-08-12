472d4d668a8495182545ebfef271e48260ee2a3f
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-12 18:49:56 +0530

    Add files via upload

---
+++ que_5.py
@@ -0,0 +1,13 @@
+age = int(input("Enter age : "))
+
+if age > 18:
+    print("You can vote")
+    print("You are above 18 so, you can vote")
+
+elif age < 18:
+    print("You can not vote")
+    print("You are not 18 above ")
+
+else :
+    print("You are just 18")
+    print("You have to issue voting card")
