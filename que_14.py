26bbcf2d0217c75376261c7c09b5317ceaa47496
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-12 20:21:10 +0530

    Add files via upload

---
+++ que_14.py
@@ -0,0 +1,12 @@
+password = "abcd1234"
+
+enter_password = input("Enter the password : ")
+
+while password != enter_password:
+    print("wrong Password")
+    enter_password = input("Enter password : ")
+
+print("Correct password")
+
+
+
