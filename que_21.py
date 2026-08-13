11083d86c72b9262e39d78d5810cf2cfc332670e
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-13 21:22:16 +0530

    Add files via upload

---
+++ que_21.py
@@ -0,0 +1,5 @@
+text = "python programming"
+print("First 6 characters : ",text[:6])
+print("Last 6 characters : ",text[-6:])
+print("Every second character from the string : ",text[1::2])
+print("Reverse string : ",text[::-1])
---
+++ que_23.py
@@ -0,0 +1,5 @@
+text = "123abc"
+if text.isalnum():
+    print("It is alphanumeric")
+else :
+    print("It is not a alphanumeric")
