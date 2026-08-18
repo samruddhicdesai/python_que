e8c71286764821eb3ce0c81e30d2158e7d9bd582
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-18 22:58:34 +0530

    Add files via upload

---
+++ que_42.py
@@ -0,0 +1,16 @@
+marks = int(input("Enter Marks : "))
+
+if (marks<=100 and marks>=90):
+    print("A")
+elif (marks<=89 and marks>=80):
+    print("B")
+elif (marks<=79 and marks>=70):
+    print("C")
+elif (marks<=69 and marks>=60):
+    print("D")
+elif (marks <= 60):
+    print("F")
+else:
+    print("Enter numbers between 1 to 100.")
+    print("As you get marks betwwen 1 to 100.")
+
