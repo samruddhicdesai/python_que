cacf56435266a0b855b7a407bf2002b3a6b69c25
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-22 11:31:03 -0700

    Add files via upload

---
+++ que_40.py
@@ -0,0 +1,10 @@
+student = {"name":"John",
+           "age":20,
+           "grade":"A"}
+print(student["name"])
+print(student["grade"])
+student["grade"] = "A+"
+print(student["grade"])
+student["city"]="Delhi"
+print(student["city"])
+print(student)
