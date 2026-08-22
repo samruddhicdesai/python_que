cadedfb97c5ef239857b31298fc6664bb0170068
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-22 11:32:20 -0700

    Add files via upload

--- que_42.py
+++ que_42.py
@@ -1,16 +1,5 @@
-marks = int(input("Enter Marks : "))
+numbers = [1,2,3,4,5,6,7,7,8,8,4,5,2,1,4]
 
-if (marks<=100 and marks>=90):
-    print("A")
-elif (marks<=89 and marks>=80):
-    print("B")
-elif (marks<=79 and marks>=70):
-    print("C")
-elif (marks<=69 and marks>=60):
-    print("D")
-elif (marks <= 60):
-    print("F")
-else:
-    print("Enter numbers between 1 to 100.")
-    print("As you get marks betwwen 1 to 100.")
+result = set(numbers)
 
+print(list(result))
