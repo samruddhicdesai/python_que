60f883925eb6561cdfd1974c479149eb20026158
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-25 23:17:55 +0530

    Add files via upload

---
+++ que_99.py
@@ -0,0 +1,9 @@
+def prime(number):
+    if number < 2:
+        return False
+    for i in range(2,int(number**0.5)+1):
+        if number % i == 0:
+            return False
+    return True
+print(prime(7))
+        
