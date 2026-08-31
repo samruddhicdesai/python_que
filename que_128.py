53170f9efcce7c3bb611985d74b7a681be90e96f
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-31 22:51:50 +0530

    Add files via upload

---
+++ que_128.py
@@ -0,0 +1,7 @@
+number = [1,2,3,4,5,7,7,6,5,4,3,2,1]
+
+for num in number:
+    result = set(number)
+    final = list(result)
+
+print(final)
---
+++ que_129.py
@@ -0,0 +1,5 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+
+for nums in list:
+    if  nums > min(list) and nums < max(list):
+         print(nums)
